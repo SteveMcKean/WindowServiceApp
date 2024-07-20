e1423cc58c9941ed88d73c50eaf45f991a6e14c1
Steve McKean <steven.mckean@peraton.com>
2024-07-20 15:32:48 -0500

    Remaining items.

---
+++ WpfApp1/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WpfApp1;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    public static IServiceProvider ServiceProvider { get; private set; }
+    
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        
+        var serviceCollection = new ServiceCollection();
+        ConfigureServices(serviceCollection);
+       
+        ServiceProvider = serviceCollection.BuildServiceProvider();
+        
+        ViewModelLocator.ServiceProvider = ServiceProvider;
+        DialogService.ServiceProvider = ServiceProvider;
+        
+        var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+        mainWindow.Show();
+        
+    }
+
+    private void ConfigureServices(ServiceCollection serviceCollection)
+    {
+        serviceCollection.AddSingleton<IDialogService, DialogService>();
+        serviceCollection.AddSingleton<MainWindowViewModel>();
+        serviceCollection.AddSingleton<MainWindow>();
+        serviceCollection.AddSingleton<ITimeService, TimeService>();
+        serviceCollection.AddTransient<NotificationViewModel>();
+        serviceCollection.AddSingleton<IEventAggregator, EventAggregator>();
+        
+        DialogService.RegisterDialog<Notification, NotificationViewModel>();
+       
+    }
+}
